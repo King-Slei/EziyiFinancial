6abcf4bbaaaf33636627de639b52f7a82a849cfc
King-Slei <149031080+King-Slei@users.noreply.github.com>
2026-02-13 22:52:29 +0100

    Update Footer.tsx

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -205,18 +205,12 @@ We may update these Terms at any time. Continued use of our services after chang
             onClick={closePopup}
           >
             <div
-  className="bg-white text-black max-w-3xl w-full p-6 rounded-lg overflow-y-auto max-h-[80vh] self-start mt-4"
+  className="bg-white text-black max-w-3xl w-full p-6 rounded-lg overflow-y-auto max-h-[80vh]"
   onClick={(e) => e.stopPropagation()}
 >
-  <button
-    onClick={closePopup}
-    className="float-right text-red-500 font-bold text-lg"
-  >
-    ×
-  </button>
+  <button onClick={closePopup} className="float-right text-red-500 font-bold text-lg">×</button>
   <pre className="whitespace-pre-wrap">{popupContent}</pre>
-</div>
-          </div>
+</div>  </div>
         )}
       </footer>
     </>
